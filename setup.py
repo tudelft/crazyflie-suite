1ee866ce098134f0dd137b643a256b28db3556d4
Huizerd <jessehagenaars@gmail.com>
2020-07-07 20:53:43 +0200

    Auto stripout for notebooks

--- setup.py
+++ setup.py
@@ -3,5 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name="crazyflie-suite",
     packages=find_packages(),
-    install_requires=["cflib", "numpy", "pandas", "pyyaml", "pre-commit"],
+    install_requires=[
+        "cflib",
+        "numpy",
+        "pandas",
+        "matplotlib",
+        "jupyterlab",
+        "pyyaml",
+        "pre-commit",
+    ],
 )
